d71c71c49efeba98e3e5cf36a0c87a6f0eb2bc34
moraadiana <moraadiana13@gmail.com>
2023-04-20 16:33:38 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *main - Using put function to printout a line
+ *Return:0 means success
+ */
+int main(void)
+{
+	puts("\" Programming is like building a multilingual puzzle");
+	return (0);
+}
