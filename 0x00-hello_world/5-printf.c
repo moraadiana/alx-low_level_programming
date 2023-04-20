cbf3135ab154f4034bf30581655cf28ec14e4940
moraadiana <moraadiana13@gmail.com>
2023-04-20 16:47:16 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print using printf
+ * return: 0 (succes)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n');
+return(0);
+}
