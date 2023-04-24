df3cc48f445fb0a288de576e0672a6cf2c990cee
moraadiana <moraadiana13@gmail.com>
2023-04-24 06:57:30 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <time.h>
+
+/**
+ * main - entry point
+ * Return: always 0
+ */
+
+int main(void)
+{       
+	char r;
+
+ 	for (r = 'z';  >= 'a'; r--)
+	putchar(r);
+	putchar('\n');
+
+	return (0);
+
+}
+
