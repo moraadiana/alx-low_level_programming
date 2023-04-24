0a1666663a4d8a78f1a1fe23ee95d4e5f33cdc5a
moraadiana <moraadiana13@gmail.com>
2023-04-24 05:54:49 -0700

    the alphabet game

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,8 +7,8 @@ int main(void)
 {
         char x;
 
-        for (x = 'a' ; x <='z'; x++)
-                putchar(x);
-                putchar('\n');	
-return (0);
+        for (x = 'a'; x <='z'; x++)
+	putchar(x);
+	putchar('\n');
+	return (0);
 }
