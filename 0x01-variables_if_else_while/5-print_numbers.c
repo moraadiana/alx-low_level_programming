4b4a0d0754b9248b21ebe976e12ba32b9c1052cf
moraadiana <moraadiana13@gmail.com>
2023-04-24 06:41:00 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <Stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int a;
+	for (a = 0; a < 10; a++)
+	printf("%d", a);
+	printf("\n");
+
+	return (0);
+ 
+}
+
