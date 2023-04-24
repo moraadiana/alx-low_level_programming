23672acd18509366d1dda83d9d913b5d1cf99267
moraadiana <moraadiana13@gmail.com>
2023-04-24 06:50:58 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * Return: always 0
+ */
+
+int main(void)
+{
+
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	putchar(x);
+	putchar('\n');
+
+	return(0);
+}
