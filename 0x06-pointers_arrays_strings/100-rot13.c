89297ed4b3bee06d4d01d7641181bff0cf5ba989
moraadiana13 <moraadiana13@gmail.com>
2023-06-29 12:18:09 -0700

    more pointers

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * char *rot13(char *) -  encodes a string using rot13
+ * return: str
+ */
+
+char *rot13(char *str)
+{
+	int i, j;
+	char *alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *rot13_alphabet = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; alphabet[j] != '\0'; j++)
+		{
+			if (str[i] == alphabet[j])
+			{
+				str[i] = rot13_alphabet[j];
+				break;
+			}
+		}
+	}
+	return str;
+}
