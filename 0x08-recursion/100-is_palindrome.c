36ab9cb8769e70f68a597cb754e599c2ab124364
moraadiana13 <moraadiana13@gmail.com>
2023-07-06 08:36:06 -0700

    recurssion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,6 @@
 #include "main.h"
 int check_pal(char *s, int i, int len);
-int _strlen_recursioon(char *s);
+int _strlen_recursion(char *s);
 
 /**
  * is_palindrome - check if a string is a palindrome
