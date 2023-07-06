6f9de07c35276a7c78adf0054cda905330727375
moraadiana13 <moraadiana13@gmail.com>
2023-07-06 06:13:54 -0700

    recussion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns Length of String
+ * @s: string
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+	if (*s)
+	{
+		i++;
+		i+=_strlen_recursion(s + 1);
+	}
+	return (i);
+}
