0d8d27dba65845ba38e70d5ee06f81cb533e1958
moraadiana13 <moraadiana13@gmail.com>
2023-07-21 10:48:01 -0700

    function pointerd

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
-#ifndef function_pointers.h
-#define function_pointers.h
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+#include <stddef.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
