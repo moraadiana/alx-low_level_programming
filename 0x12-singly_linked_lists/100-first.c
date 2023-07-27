4e3e936833157876f67a9c4f4149f8ee8af540e5
moraadiana13 <moraadiana13@gmail.com>
2023-07-27 02:37:49 -0700

    Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,6 +5,7 @@
 /* Function to be executed before main */
 void __attribute__((constructor)) before_main()
 {
-    printf("You're beat! and yet, you must allow,\n I bore my house upon my back!\n");
+    printf("You're beat! and yet, you must allow,\n"
+		   " I bore my house upon my back!\n");
 }
 
