15c7237ca177651da5fff5fb05e741af79a331e3
moraadiana13 <moraadiana13@gmail.com>
2023-07-27 02:24:30 -0700

    singly data stractures

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/* Function to be executed before main */
+void __attribute__((constructor)) before_main()
+{
+    printf("You're beat! and yet, you must allow,\n I bore my house upon my back!\n");
+}
+
